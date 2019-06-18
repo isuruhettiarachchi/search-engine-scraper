7aee56b43d937d13a921c85d1e601b4a68c3d647
Isuru Hettiarachchi <isuru71@gmail.com>
2019-06-18 23:00:29 +0530

    Use Apify to scrape sites from se-scraper links

--- src/routes/search.js
+++ src/routes/search.js
@@ -3,19 +3,34 @@ const router = express.Router();
 
 const se_scraper = require('se-scraper');
 
+const scraper = require('../scraper/tutorialspoint-scraper');
+
 // const scraper = require('../scraper/scraper');
 
 
 
 router.get('/', function(req, res, next) {
+    let searchKey = req.query.keywords + ' site:www.tutorialspoint.com';
+
     let scrape_job = {
         search_engine: 'google',
-        keywords: [req.query.keywords + ' site:www.tutorialspoint.com'],
+        keywords: [searchKey],
         num_pages: 1,
     };
 
     se_scraper.scrape({}, scrape_job).then(data => {
-        res.send(data);
+        console.log('links scrapped from se-scraper');
+        
+        let searchResults = data.results[searchKey][1].results;
+        let urls = searchResults.map(obj => {
+            let url = { url: obj.link };
+            return url;
+        });
+
+        scraper(urls, (response) => {
+            res.send(response);
+        })
+        // res.send(urls);
     });
     // res.send(results);
 })
--- src/scraper/tutorialspoint-scraper.js
+++ src/scraper/tutorialspoint-scraper.js
@@ -1,62 +1,60 @@
 const Apify = require('apify');
 
-Apify.main(async () => {
+const scrapper = (sources, callback) => {
+    console.log('Apify scraper called');
 
-    const requestList = new Apify.RequestList({
-        sources: [
-            { url: 'https://www.tutorialspoint.com/cplusplus/cpp_object_oriented.htm' },
-            { url: 'https://www.tutorialspoint.com/OOPS-concepts-in-Java' },
-            { url: 'https://www.tutorialspoint.com/What-is-object-oriented-programming-OOP' },
-            { url: 'https://www.tutorialspoint.com/What-are-basic-Object-oriented-programming-concepts' },
-            { url: 'https://www.tutorialspoint.com/php/php_object_oriented.htm' },
-            { url: 'https://www.tutorialspoint.com/java_essential_training/java_oops_concepts.asp' },
-            { url: 'https://www.tutorialspoint.com/object_oriented_analysis_design/ooad_object_oriented_principles.htm' },
-            { url: 'https://www.tutorialspoint.com/What-are-Python-OOP-Basics' },
-            { url: 'https://www.tutorialspoint.com/object_oriented_analysis_design/ooad_object_oriented_paradigm.htm' },
-            { url: 'https://www.tutorialspoint.com/java/java_encapsulation.htm' }
-        ],
-    });
+    Apify.main(async () => {
 
-    await requestList.initialize();
+        let links = {
+            sources: sources
+        };
 
-    const crawler = new Apify.CheerioCrawler({
-        requestList,
-        minConcurrency: 10,
-        maxConcurrency: 50,
-        maxRequestRetries: 1,
-        handlePageTimeoutSecs: 60,
-
-        handlePageFunction: async ({ request, html, $ }) => {
-            console.log(`Processing ${request.url}...`);
-
-            const title = $('title').text();
-            const h1texts = [];
-            const para = [];
-            $('h1').each((index, el) => {
-                h1texts.push({
-                    text: $(el).text(),
+        const requestList = new Apify.RequestList(links);
+    
+        await requestList.initialize();
+    
+        const crawler = new Apify.CheerioCrawler({
+            requestList,
+            minConcurrency: 10,
+            maxConcurrency: 50,
+            maxRequestRetries: 1,
+            handlePageTimeoutSecs: 60,
+    
+            handlePageFunction: async ({ request, html, $ }) => {
+                console.log(`Processing ${request.url}...`);
+    
+                const title = $('title').text();
+                const h1texts = [];
+                const para = [];
+                $('h1').each((index, el) => {
+                    h1texts.push({
+                        text: $(el).text(),
+                    });
                 });
-            });
-
-            $('p').each((index, el) => {
-                para.push($(el).text());
-            });
-
-            await Apify.pushData({
-                url: request.url,
-                title,
-                h1texts,
-                para
-            });
-
-        },
-
-        handleFailedRequestFunction: async ({ request }) => {
-            console.log(`Request ${request.url} failed twice.`);
-        },
+    
+                $('p').each((index, el) => {
+                    para.push($(el).text());
+                });
+    
+                await Apify.pushData({
+                    url: request.url,
+                    title,
+                    h1texts,
+                    para
+                });
+    
+            },
+    
+            handleFailedRequestFunction: async ({ request }) => {
+                console.log(`Request ${request.url} failed twice.`);
+            },
+        });
+    
+        await crawler.run();
+    
+        console.log('Crawler finished.');
+        callback('finished');
     });
+}
 
-    await crawler.run();
-
-    console.log('Crawler finished.');
-});
+module.exports = scrapper;
